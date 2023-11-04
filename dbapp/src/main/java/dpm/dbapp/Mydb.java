ecd697624de63cc3d8f30bc4bbf7dd83cf95296c
tulsi2905 <149880472+tulsi2905@users.noreply.github.com>
2023-11-04 19:20:07 +0530

    Add files via upload

---
+++ dbapp/src/main/java/dpm/dbapp/Mydb.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dpm.dbapp;
+import java.sql.*;
+
+/**
+ *
+ * @author sspmcoe
+ */
+public class Mydb {
+    Connection connection;
+    PreparedStatement st;
+    ResultSet rs1,rs2;
+    
+    Mydb()
+    {
+        try {       
+             Class.forName("com.mysql.jdbc.Driver");
+                    connection = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/employee",
+                        "root", "");
+               System.out.println("Connect");
+            } catch (Exception e) {
+                    System.out.println(e);
+                }
+            
+    }
+    public void insert(int empid,String empname,String leavetype,String leavedate,String charge,String indate)
+    {
+        try {
+            st = (PreparedStatement) connection
+                        .prepareStatement("INSERT INTO `empleave`  VALUES ('"+empid + "', '" +empname +"', '" +leavetype +"', '"+leavedate +"', '"+charge +"', '"+indate + "')");
+            System.out.println(st.execute());
+            /*if(st.execute())
+            {
+                System.out.print("Insert");
+                //JOptionPane.showMessageDialog(Mydb, "You have successfully inserted");
+            } else {
+                        //JOptionPane.showMessageDialog(Mydb, "Wrong input");
+                        System.out.print("Insert failed");
+                    }*/
+        }
+        catch (SQLException sqlException) {
+                    sqlException.printStackTrace();
+                }
+    }
+    public static void main(String args[])
+    {
+        Mydb d =new Mydb();
+        d.insert(1, "tulsi","sick","27-Oct-2023","anushka","30-Oct-2023");
+    }
+    
+}
